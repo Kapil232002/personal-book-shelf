6543d12cb0d47351f3fd73d6731c3e9aa0efbabf
kapil kumar <kapilkumar232222@gmail.com>
2025-06-29 18:16:53 +0530

    include the databse in setting

--- book_manager/settings.py
+++ book_manager/settings.py
@@ -75,12 +75,30 @@ WSGI_APPLICATION = 'book_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import os
+
+DEBUG = os.environ.get("DEBUG", "True") == "True"  # True for local, False for Render
+
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ["DB_NAME"],
+            "USER": os.environ["DB_USER"],
+            "PASSWORD": os.environ["DB_PASSWORD"],
+            "HOST": os.environ["DB_HOST"],
+            "PORT": os.environ.get("DB_PORT", "5432"),
+        }
+    }
+
+
 
 
 # Password validation
